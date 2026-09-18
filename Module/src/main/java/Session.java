c8f723b3a643e0b640172f8949911fe2acbd0dac
calpolyJacky <jwu283@calpoly.edu>
2026-09-17 18:36:58 -0700

    added basic session info

--- Module/src/main/java/Session.java
+++ Module/src/main/java/Session.java
@@ -1,3 +1,22 @@
 public class Session {
+    public int id;
+    public String title;
+    public String mentor;
+    public String date;
+    public String location;
+    public int maxParticipants;
 
+    public Session(int id, String title, String mentor, String date, String location, int maxParticipants) {
+        this.id = id;
+        this.title = title;
+        this.mentor = mentor;
+        this.date = date;
+        this.location = location;
+        this.maxParticipants = maxParticipants;
+
+    }
+
+    public String toString() {
+        return "Session info: " + date + id + title + mentor + location + maxParticipants;
+    }
 }
