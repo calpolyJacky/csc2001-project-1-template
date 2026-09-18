3e6642f1b908be7e0ff797e61950d88f48c45e5a
calpolyJacky <jwu283@calpoly.edu>
2026-09-17 20:10:56 -0700

    updated session

--- Module/src/main/java/Session.java
+++ Module/src/main/java/Session.java
@@ -5,18 +5,34 @@ public class Session {
     public String date;
     public String location;
     public int maxParticipants;
+    public int participants;
 
-    public Session(int id, String title, String mentor, String date, String location, int maxParticipants) {
+    public Session(int id, String title, String mentor, String date,
+                   String location, int maxParticipants) {
         this.id = id;
         this.title = title;
         this.mentor = mentor;
         this.date = date;
         this.location = location;
         this.maxParticipants = maxParticipants;
+        this.participants = 0;
+    }
 
+    public boolean registerParticipant() {
+        if (participants >= maxParticipants) {
+            return false;
+        }
+
+        participants++;
+        return true;
     }
 
     public String toString() {
-        return "Session info: " + date + id + title + mentor + location + maxParticipants;
+        return "Session ID: " + id +
+                "\nTitle: " + title +
+                "\nMentor: " + mentor +
+                "\nDate: " + date +
+                "\nLocation: " + location +
+                "\nParticipants: " + participants + "/" + maxParticipants;
     }
 }
